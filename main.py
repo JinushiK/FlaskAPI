b3a32e5a35f858670e7077ab5d5203b9ae8b829d
JinushiK <jinushitirani1467@gmail.com>
2021-12-02 16:20:56 +0530

    Implemented a FlaskAPI

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 from flask import Flask
+
 app=Flask(__name__)
+
 @app.route('/')
 def hello():
     return 'HELLO'
+
+@app.route('/Home')
+def bye():
+    return 'Bye'
+
 if __name__=='__main__':
     app.run()
