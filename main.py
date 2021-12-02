dfc9ef2a2398406120339b4de7d809514d0cd8ba
JinushiK <jinushitirani1467@gmail.com>
2021-12-02 16:35:00 +0530

    Implemented a FlaskAPI

--- main.py
+++ main.py
@@ -10,5 +10,13 @@ def hello():
 def bye():
     return 'Bye'
 
+class Test:
+    pass
+Test.x=45
+Test.foo=lambda self: print("Hello")
+myobj=Test()
+print(myobj.x)
+myobj.foo()
+
 if __name__=='__main__':
     app.run()
